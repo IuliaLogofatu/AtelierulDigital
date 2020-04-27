9a02596c933b1ae06dc25f5b8b2d6cadc2c34c60
unknown <logofatu.iuliaa@gmail.com>
2020-04-27 15:35:36 +0300

    first commit

---
+++ src/AnimalHierarchy/Main.java
@@ -0,0 +1,15 @@
+package AnimalHierarchy;
+
+public class Main {
+
+    public static void main(String [] args) {
+        Fish d = new Fish(0, "Ion");
+        System.out.println(d);
+        Cat c = new Cat(4, "Fluffy");
+        Animal a = new Fish(0, "Mih");
+        Animal e = new Spider(8);
+        Pet p = new Cat(4, "bla");
+        System.out.println("orice");
+    }
+
+}
